52430004fdd49377f5835a322dc839c432f68426
huzeyfereis <huzeyfereis@hotmail.com>
2019-02-23 15:05:34 +0000

    asdasda

--- week-1-practise/level-2/4-currency-conversion-2.js
+++ week-1-practise/level-2/4-currency-conversion-2.js
@@ -7,8 +7,33 @@
   Find a way to add 1% to all currency conversions (think about the DRY principle)
 */
 
-function convertToUSD(price) {}
-function convertToBRL(price) {}
+function calculateTax(price) {
+  return price * 1.01;
+}
+
+function calculateCurrency(price, currency) {
+  let response = 0;
+  if (currency === "dollar") {
+    response = price * 2;
+  } else {
+    response = price / 2;
+  }
+  return response;
+}
+
+function convertToUSD(real) {
+  const dollar = calculateCurrency(real, "real");
+  const afterTax = calculateTax(dollar);
+  return afterTax;
+
+  // return calculateTax(real / 2);  -  Bu üstteki kodların kısa hali
+}
+function convertToBRL(dollar) {
+  //return dollar * 2 * 1.01;
+  const real = calculateCurrency(dollar, "dollar");
+  const afterTax = calculateTax(real);
+  return afterTax;
+}
 
 /* DO NOT EDIT BELOW THIS LINE */
 /* --------------------------- */
@@ -16,6 +41,7 @@ function convertToBRL(price) {}
 var product = "The life and times of Mozafar Haider";
 var price = 12.5;
 var priceInUSD = convertToUSD(price);
+
 var priceInBRL = convertToBRL(price);
 
 console.log("Product: " + product);
